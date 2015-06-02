c76ab2f3415c47bc148ac1abca13e5b230e8a6ac
Echo <bdottn.lee+account.GitHub@gmail.com>
2015-06-02 14:08:00 +0800

    Person Initial commit

---
+++ 2,000 Things You Should Know About CSharp/2,000 Things You Should Know About CSharp UnitTest/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace _2_000_Things_You_Should_Know_About_CSharp_UnitTest
+{
+    class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
