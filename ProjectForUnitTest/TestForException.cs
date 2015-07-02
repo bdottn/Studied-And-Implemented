0c863c3d959053f832d498036b21682318386c83
Echo <bdottn.lee+account.GitHub@gmail.com>
2015-07-02 14:49:42 +0800

    edit description

--- ProjectForUnitTest/TestForException.cs
+++ ProjectForUnitTest/TestForException.cs
@@ -3,9 +3,17 @@ using System;
 
 namespace ProjectForUnitTest
 {
+    // Reference：https://msdn.microsoft.com/zh-tw/library/microsoft.visualstudio.testtools.unittesting.expectedexceptionattribute.aspx
+
     [TestClass]
     public class TestForException
     {
+        /*
+         * 使用 ExpectedExceptionAttribute 表示測試方法執行期間所發生的預期例外狀況。
+         * 使用方法為在 Method 上加上 [ExpectedException(exceptionType)]
+         * 如果擲回預期的 exceptionType，則測試方法將會成功。
+         */
+
         class DivisionClass
         {
             public int Divide(int numerator, int denominator)
@@ -16,7 +24,7 @@ namespace ProjectForUnitTest
 
         [TestMethod()]
         [ExpectedException(typeof(DivideByZeroException))]
-        public void DivideTest()
+        public void DivideTest_InputZeroDenominator_ReturnDivideByZeroException()
         {
             DivisionClass target = new DivisionClass();
 
