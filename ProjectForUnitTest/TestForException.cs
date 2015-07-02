f4c08885280050fa8fb16462f3d88559f32bd382
Echo <bdottn.lee+account.GitHub@gmail.com>
2015-07-02 14:42:22 +0800

    TestForException Implement

---
+++ ProjectForUnitTest/TestForException.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProjectForUnitTest
+{
+    [TestClass]
+    public class TestForException
+    {
+        class DivisionClass
+        {
+            public int Divide(int numerator, int denominator)
+            {
+                return numerator / denominator;
+            }
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivideTest()
+        {
+            DivisionClass target = new DivisionClass();
+
+            int numerator = 4;
+            int denominator = 0;
+            int actual;
+
+            // throw DivideByZeroException
+            actual = target.Divide(numerator, denominator);
+        }
+    }
+}
